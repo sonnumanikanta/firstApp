2d56f6025fc6d5c664ff12d956c335e8db93fdeb
sonnumanikanta <138505104+sonnumanikanta@users.noreply.github.com>
2026-03-24 15:30:32 +0530

    pdf_utilis.py

--- resume_app/pdf_utilis.py
+++ resume_app/pdf_utilis.py
@@ -9,21 +9,25 @@
 #     HTML(string=html_content).write_pdf(temp_file.name)
 
 #     return temp_file.name
+
 from xhtml2pdf import pisa
-import os
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 import uuid
+from io import BytesIO
 
 def generate_pdf_from_html(html_content):
     file_name = f"{uuid.uuid4()}.pdf"
-    file_path = os.path.join("temp", file_name)
 
-    # create temp folder if not exists
-    os.makedirs("temp", exist_ok=True)
+    pdf_buffer = BytesIO()
 
-    with open(file_path, "w+b") as pdf_file:
-        pisa_status = pisa.CreatePDF(html_content, dest=pdf_file)
+    pisa_status = pisa.CreatePDF(html_content, dest=pdf_buffer)
 
     if pisa_status.err:
         return None
 
-    return file_path
+    # save to R2 (not local temp folder)
+    default_storage.save(file_name, ContentFile(pdf_buffer.getvalue()))
+
+    # return URL instead of file path
+    return default_storage.url(file_name)
